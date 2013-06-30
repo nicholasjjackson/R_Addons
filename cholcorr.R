a7eba6a8ecebe1b00818fbd45be521ae4aa30a23
Xiao He <praguewatermelon@gmail.com>
2013-06-30 16:30:26 -0700

    Update cholcorr.R
    Changed the print format and make the output invisible.

--- cholcorr.R
+++ cholcorr.R
@@ -1,10 +1,8 @@
-cholcorr<-function(cmatrix, obs=1000, seed=5,...){
+cholcorr<-function(cmatrix, obs=1000, seed=5, printit=TRUE,...){
 
 	#Determine if Seed needs to be Set
   		if(seed)
     	 	  set.seed(seed)
-
-
 	##Determin Length of Correlation Martix
 		row=nrow(cmatrix)
 		col=ncol(cmatrix)	
@@ -13,32 +11,32 @@ cholcorr<-function(cmatrix, obs=1000, seed=5,...){
 
 		#Cholesky Decomposition of C Matrix
 			U=chol(cmatrix)
-		
+
 		#Uncorrelated Normally Distributed Random variables 
 			R=matrix(rnorm(obs*col), nrow=obs)
 
 		#Matrix of Correlated Variables 
 			M=R %*% U
-	
+
 		#Store as a data frame
 			output=data.frame(M)
 			rdata=data.frame(R)
-	
+
 		#Show Correlations of the now correlated variables
-			print("Observed Correlation")
-			print(cor(output))
-	
+		if(printit){
+			cat("Observed Correlation\n\n")
+			cat("Seed: ",seed,"\n")
+			cat("Number of observations: ", obs, "\n\n")
+			output.print = cor(output)
+			output.print = apply(output.print, 2, format, digits=4, nsmall=TRUE)
+			print(as.data.frame(output.print))
+		}
 		#Store as list
-			list(cordata=output, randata=rdata, obscor=cor(output), obs=obs, seed=seed)
+			invisible(list(cordata=output, randata=rdata, obscor=cor(output), obs=obs, seed=seed))
 
 	}
 	else {
-		print("Correlation Matrix must be of dimensions n X n")
+		stop("Correlation Matrix must be of dimensions n X n")
 	}
 
 }
-
-
-
-
-
